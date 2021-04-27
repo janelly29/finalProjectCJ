d7bbec2e26fad824b37dde67ba3ee1a4a9bd36f7
‘Cassjackson39’ <cassjackson39@gmail.com>
2021-04-27 16:37:01 -0500

    Added buttons for plotting

--- finalProject.m
+++ finalProject.m
@@ -20,7 +20,23 @@ function[] = finalProject()
     plotgui.yAxisTitleBox = uicontrol('style', 'text', 'string', 'ln of Concentration', 'units', 'normalized' , 'position', [0.002 0.5 0.1 0.05]); 
     plotgui.xValuesBox = uicontrol('style', 'edit', 'string', 'Enter Time Values', 'units', 'normalized' , 'position', [0.2 0.02 0.1 0.05]); 
     plotgui.yValuesBox = uicontrol('style', 'edit', 'string', 'Enter Concentration Values', 'units', 'normalized' , 'position', [0.01 0.85 0.1 0.05]); 
-   
+   %x and y value boxes that can be edited
+    plotgui.yValues = uicontrol('style','edit','units','normalized','position',[0.35,0.01,0.1,0.05]);
+    plotgui.xValues = uicontrol('style','edit','units','normalized','position',[0.65,0.01,0.1,0.05]);
+    
+    %button for first order equation
+    plotgui.firstorderButton = uicontrol('style','pushbutton','string','FirstOrder',
+    %button for second order equation
+    %button for third order equation
+    function  [] = plotvaluesCallBack(~,~)%plot the values from first order equation
+        if ~isempty(plotgui.xValues.String) || ~isempty(plotgui.yValues.String)%need to have values entered
+            xVal = str2num(plotgui.xValues.String);
+            yVal = str2num(plotgui.yValues.String);
+        else
+            return;
+        end
+        
+    end
   
 end
 
